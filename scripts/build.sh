eb2cbf37981e6488d3d538b97be5289f524ef125
Kajeepan Kandeepan <kajeepan.kandeepan@gmail.com>
2024-07-30 09:23:17 +0000

    breaking: fix pipeline

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
 
-export VERSION=0.0.1
-export IMG_TAG_BASE=docker.io/kallepan/rq-operator
-export IMG=$IMG_TAG_BASE:$VERSION
+# exit on error
+set -e
+
+VERSION=0.0.1
+IMG_TAG_BASE=docker.io/kallepan/rq-operator
 CATALOG_IMG_BASE=docker.io/kallepan/homelab
+RQ_OPERATOR_BUNDLE_TAG=0.0.1
 
 # Build the manager binary
 cd rq-operator
 
 # Generate manifests e.g. CRD, RBAC etc.
-make generate manifests
+# make generate manifests
 
 # Build the docker image
-make docker-build docker-push
+IMG=$IMG_TAG_BASE:$VERSION
+make docker-build docker-push IMG=$IMG
 
 # Build the bundle
 BUNDLE_IMG=${IMG_TAG_BASE}-bundle:$VERSION
-make bundle bundle-build bundle-push BUNDLE_IMG=$BUNDLE_IMG
+make bundle IMG=$BUNDLE_IMG VERSION=$VERSION
+make bundle-build bundle-push BUNDLE_IMG=$BUNDLE_IMG
 
 # Build the catalog
+cd ../catalog
+
 CATALOG_IMG=${CATALOG_IMG_BASE}-catalog:$VERSION
-make catalog-build catalog-push BUNDLE_IMGS=$BUNDLE_IMG CATALOG_IMG=$CATALOG_IMG
+sed -i s/RQ_OPERATOR_BUNDLE_TAG/$RQ_OPERATOR_BUNDLE_TAG/g operator-template.yaml
+opm alpha render-template semver -o yaml < operator-template.yaml > homelab-catalog/catalog.yaml
+docker build --platform linux/amd64 -f homelab-catalog.Dockerfile -t $CATALOG_IMG .
+docker push $CATALOG_IMG
 
 # operator-sdk run bundle $BUNDLE_IMG 
